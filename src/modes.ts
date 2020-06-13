29b643faed9a9dbdd053e408061c4214f98d3e8d
Jeff Cooper <jeff@cuperman.net>
2020-06-13 07:29:07 -0700

    More modes

--- src/modes.ts
+++ src/modes.ts
@@ -1,14 +1,23 @@
+// reference: https://en.wikipedia.org/wiki/Mode_(music)
+
 import { Note } from './notes';
 import { Step } from './steps';
 
 export type Mode = Note[];
 
-function reduceSteps(mode: Mode, step: Step, index: number) {
-  return [...mode, mode[index] + step];
+function makeMode(root: Note, steps: Step[]): Mode {
+  return steps
+    .reduce(
+      (mode, step, index) => {
+        return [...mode, mode[index] + step];
+      },
+      [root]
+    )
+    .slice(0, steps.length);
 }
 
 export function ionianMode(root: Note): Mode {
-  const steps = [
+  return makeMode(root, [
     Step.WHOLE,
     Step.WHOLE,
     Step.HALF,
@@ -16,6 +25,77 @@ export function ionianMode(root: Note): Mode {
     Step.WHOLE,
     Step.WHOLE,
     Step.HALF
-  ];
-  return steps.reduce(reduceSteps, [root]).slice(0, steps.length);
+  ]);
+}
+
+export function dorianMode(root: Note): Mode {
+  return makeMode(root, [
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE
+  ]);
+}
+
+export function phrygianMode(root: Note): Mode {
+  return makeMode(root, [
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE
+  ]);
+}
+
+export function lydianMode(root: Note): Mode {
+  return makeMode(root, [
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF
+  ]);
+}
+
+export function mixolydianMode(root: Note): Mode {
+  return makeMode(root, [
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE
+  ]);
+}
+
+export function aeolianMode(root: Note): Mode {
+  return makeMode(root, [
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE
+  ]);
+}
+
+export function locrianMode(root: Note): Mode {
+  return makeMode(root, [
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.HALF,
+    Step.WHOLE,
+    Step.WHOLE,
+    Step.WHOLE
+  ]);
 }
